c89e913920f60dafc591d9ca6f0c6103d8e5620d
ArchisDhakne <archisdhakne@gmail.com>
2025-04-11 20:05:04 +0530

    Fix: added cors link from vercel

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ import applicationRoutes from "./routes/applicationRoutes.js";
 dotenv.config()
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://workbridge-five.vercel.app/", // allow your frontend
+  credentials: true,
+}))
 app.use(express.json())
 app.use(urlencoded({extended:true}));
 
